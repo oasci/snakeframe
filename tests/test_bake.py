4d25a72a17f8858bf94f8346c736251750824587
Alex Maldonado <alex@oasci.org>
2023-11-21 11:17:08 -0500

    Write to tmpdir

--- tests/test_bake.py
+++ tests/test_bake.py
@@ -1,16 +1,13 @@
 import os
-import shutil
+import tempfile
 
 import pytest
 from cookiecutter.main import cookiecutter
 
 REPO_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-TMP_DIR = "./tests/tmp/"
-BAKE_DIR = os.path.join(TMP_DIR, "python_project")
 
 
 @pytest.mark.order(0)
 def test_template_gen():
-    if os.path.exists(BAKE_DIR):
-        shutil.rmtree(BAKE_DIR)
-    cookiecutter(REPO_PATH, no_input=True, output_dir=TMP_DIR)
+    tmpdir = tempfile.TemporaryDirectory()
+    cookiecutter(REPO_PATH, no_input=True, output_dir=tmpdir.name)
