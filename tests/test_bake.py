806cde460563ba2e1490dc1fd6034bfe08cc6804
Alex Maldonado <alex@oasci.org>
2025-05-17 14:39:41 -0400

    chore: update deps

--- tests/test_bake.py
+++ tests/test_bake.py
@@ -1,13 +1,11 @@
 import os
 import tempfile
 
-import pytest
 from cookiecutter.main import cookiecutter
 
 REPO_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
-@pytest.mark.order(0)
 def test_template_gen():
     tmpdir = tempfile.TemporaryDirectory()
     cookiecutter(REPO_PATH, no_input=True, output_dir=tmpdir.name)
